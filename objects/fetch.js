5268dfa2e3e7d57350c4f1bb7aedf3310495e482
Wesley <wspencer3295@yahoo.com>
2021-03-04 21:34:39 -0700

    +create user function

--- objects/fetch.js
+++ objects/fetch.js
@@ -10,12 +10,33 @@ fetchProjects() {
   console.log(json)
   }
 
-fetchUser() {
+fetchUsers() {
     fetch(this.url)
     .then(resp => resp.json())
     .then(json => renderUser(json));
   console.log(json)
   }
+
+  createUser(user){
+    // const [firstName,lastName,email,github,linkdn,facebook,twitter,reddit,youtube] = user
+      fetch(this.url,{
+    method: "POST",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({user})
+})
+.then((response)=>{
+    return response.json();
+}).then((object)=>{
+
+    document.getElementById(`user_form`).innerHTML = ""
+    renderProjectForm()
+}).catch(function(error){
+    alert(error.message)
+})
+}
 
   renderUser(data){
       
