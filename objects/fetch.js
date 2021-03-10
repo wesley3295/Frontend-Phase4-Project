3c1bdf03b214945068940f08a542e79f135127c6
Wesley <wspencer3295@yahoo.com>
2021-03-10 11:19:50 -0700

    +fetch create projects +fetch get user&projects

--- objects/fetch.js
+++ objects/fetch.js
@@ -3,22 +3,37 @@ constructor(url){
     this.url = url
 }
 
-fetchProjects() {
+// fetchProjects() {
+//     fetch(this.url)
+//     .then(resp => resp.json())
+//     .then(json => render(json));
+//   console.log(json)
+//   }
+  
+
+  fetchUsers() {
     fetch(this.url)
     .then(resp => resp.json())
-    .then(json => renderProjects(json));
-  console.log(json)
-  }
-
-fetchUsers() {
-    fetch(this.url)
-    .then(resp => resp.json())
-    .then(json => renderUser(json));
-  console.log(json)
-  }
-
+    .then(function(json){
+      // console.log(json)
+      if(User.all.length === 0){
+        for(let i=0;i<json.data.length;i++){
+          const apiUser = json.data[i].attributes
+          const pro = json.included[i].attributes
+          
+        let u =new User([apiUser.first_name,apiUser.last_name,apiUser.email,apiUser.github,apiUser.linkdn,apiUser.facebook,apiUser.twitter,apiUser.reddit,apiUser.youtube],apiUser.id)
+        let p =new Project([pro.title,pro.video_link,pro.project_link,pro.cohort,pro.blog_link],apiUser.id,pro.id)
+        p.save()
+        u.save()
+        u.project = p
+        
+      }
+        }
+            
+  })
+}
+// creates user in api
   createUser(user){
-    // const [firstName,lastName,email,github,linkdn,facebook,twitter,reddit,youtube] = user
       fetch(this.url,{
     method: "POST",
     headers:{
@@ -26,23 +41,69 @@ fetchUsers() {
         "Accept": "application/json"
     },
     body: JSON.stringify({user})
-})
-.then((response)=>{
+  })
+  .then((response)=>{
     return response.json();
-}).then((object)=>{
-
+  }).then((object)=>{
+    
+    newUser.id = object.id
+    newUser.save()
     document.getElementById(`user_form`).innerHTML = ""
     renderProjectForm()
+})
+}
+//creates project in api
+  createProject(project){
+    
+      fetch(this.url,{
+    method: "POST",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({project})
+  })
+  .then((response)=>{
+    return response.json();
+  }).then((object)=>{
+    newProject.id = object.id
+    newProject.user_id = newUser.id
+    newUser.project = newProject
+    document.getElementById(`project_form`).innerHTML = ""
+    render()
 }).catch(function(error){
     alert(error.message)
 })
 }
+//   editUserProject(obj){
+//       fetch(this.url,{
+//     method: "POST",
+//     headers:{
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({project})
+//   })
+//   .then((response)=>{
+//     return response.json();
+//   }).then((object)=>{
+//     newProject.id = object.id
+//     newProject.user_id = newUser.id
+//     newUser.project= newProject
+//     document.getElementById(`project_form`).innerHTML = ""
+//     render()
+// }).catch(function(error){
+//     alert(error.message)
+// })
+// }
 
-  renderUser(data){
-      
-  }
-
-  renderProjects(data){
-
-  }
+// methHeadBody = (object)=>{
+// return {method: "POST",
+//   headers:{
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//   },
+//   body: JSON.stringify({object})
+// }
+// }
 }
